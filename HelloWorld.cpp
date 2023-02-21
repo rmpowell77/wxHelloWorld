bf16801eaa38d13e9592f0b065e6ff84197b2ea3
Richard Powell <rmpowell77@me.com>
2023-02-21 06:31:45 -0800

    Barebone starting point consisting of a CMakeFile.txt, and a very primative wxWindows file.

---
+++ HelloWorld.cpp
@@ -0,0 +1,24 @@
+// wxWidgets "Hello world" Program
+#include <wx/wx.h>
+
+class MyApp : public wxApp {
+public:
+    virtual bool OnInit();
+};
+
+class MyFrame : public wxFrame {
+public:
+    MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
+        : wxFrame(NULL, wxID_ANY, title, pos, size)
+    {
+    }
+};
+
+wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit()
+{
+    MyFrame* frame = new MyFrame("Hello World", wxPoint(50, 50), wxSize(450, 340));
+    frame->Show(true);
+    return true;
+}
