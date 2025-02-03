77362710eda922e52f161d3ecd437a80c047dfa3
Richard Powell <rmpowell77@me.com>
2025-02-02 16:06:31 -0800

    Allow widgets to have sizer flags a state.
    Using the name supplied.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,6 +4,7 @@
 #ifndef WX_PRECOMP
 #include <wx/wx.h>
 #endif
+#include <optional>
 
 class MyApp : public wxApp {
 public:
@@ -43,20 +44,22 @@ bool MyApp::OnInit()
 
 template <typename W>
 struct Widget {
-    explicit Widget(wxWindowID id, std::string str)
+    Widget(wxWindowID id, std::string str, std::optional<wxSizerFlags> flags = {})
         : id_(id)
         , str_(std::move(str))
+        , flags_(flags)
     {
     }
 
-    auto createAndAdd(wxWindow* parent, wxSizer* sizer, wxSizerFlags flags)
+    auto createAndAdd(wxWindow* parent, wxSizer* sizer, wxSizerFlags suppliedFlags)
     {
-        sizer->Add(new W(parent, id_, str_), flags);
+        sizer->Add(new W(parent, id_, str_), flags_.value_or(suppliedFlags));
     }
 
 private:
     wxWindowID id_ { wxID_ANY };
     std::string str_;
+    std::optional<wxSizerFlags> flags_;
 };
 
 MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
@@ -66,7 +69,7 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
 
     auto* sizerUpper = new wxBoxSizer(wxHORIZONTAL);
     Widget<wxButton> { wxID_ANY, "Click" }.createAndAdd(this, sizerUpper, wxSizerFlags().Border());
-    Widget<wxTextCtrl> { wxID_ANY, "Dog" }.createAndAdd(this, sizerUpper, wxSizerFlags(1).Border());
+    Widget<wxTextCtrl> { wxID_ANY, "Dog", wxSizerFlags(1).Border() }.createAndAdd(this, sizerUpper, wxSizerFlags().Border());
 
     topSizer->Add(sizerUpper, wxSizerFlags().Border().Expand());
 
