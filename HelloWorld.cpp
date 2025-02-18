98039e857778219a7a3be8e82a671f723f2ad667
Richard Powell <rmpowell77@me.com>
2025-02-17 18:46:40 -0800

    Using a macro to add all the unit tests.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -133,17 +133,25 @@ using TextCtrl = details::Widget<wxTextCtrl>;
 using Button = details::Widget<wxButton>;
 using Text = details::Widget<wxStaticText>;
 
+#define UNITTEST(WIDGET)                               \
+    do {                                               \
+        WIDGET {};                                     \
+        WIDGET { wxID_ANY };                           \
+        WIDGET { "Hello" };                            \
+        WIDGET { wxID_ANY, "Hello" };                  \
+        WIDGET { wxSizerFlags {} };                    \
+        WIDGET { wxID_ANY, wxSizerFlags {} };          \
+        WIDGET { "Hello", wxSizerFlags {} };           \
+        WIDGET { wxID_ANY, "Hello", wxSizerFlags {} }; \
+    } while (0)
+
 void UnitTest()
 {
-    TextCtrl {};
-    TextCtrl { wxID_ANY };
-    TextCtrl { "Hello" };
-    TextCtrl { wxID_ANY, "Hello" };
-    TextCtrl { wxSizerFlags {} };
-    TextCtrl { wxID_ANY, wxSizerFlags {} };
-    TextCtrl { "Hello", wxSizerFlags {} };
-    TextCtrl { wxID_ANY, "Hello", wxSizerFlags {} };
+    UNITTEST(TextCtrl);
+    UNITTEST(Button);
+    UNITTEST(Text);
 }
+
 }
 
 class MyApp : public wxApp {
