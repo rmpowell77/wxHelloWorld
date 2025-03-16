e23affcd85187ff0826fb11b8cdf7ac599d29402
Richard Powell <rmpowell77@me.com>
2025-03-15 20:17:02 -0700

    Adding exit button to the event table

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -137,6 +137,7 @@ wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
     EVT_MENU(ID_Hello, MyFrame::OnHello)
     EVT_MENU(wxID_EXIT, MyFrame::OnExit)
     EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
+    EVT_BUTTON(wxID_EXIT, MyFrame::OnExit)
 wxEND_EVENT_TABLE()
 
 wxIMPLEMENT_APP(MyApp);
